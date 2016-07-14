5efa807d1e938ba29e91a1fd5d0b0d6cc5203596
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-14 15:40:35 +0200

    travis: Read tag variable from argv

--- docker.sh
+++ docker.sh
@@ -1,6 +1,7 @@
 
 dnf install rpm-build -y
 cd /docker
-TRAVIS_TAG=0.0.1
-VERBOSE=* ./go-bin-rpm generate -a 386 --version ${TRAVIS_TAG} -b pkg-build/386/ -o go-bin-rpm-386.rpm
-VERBOSE=* ./go-bin-rpm generate -a amd64 --version ${TRAVIS_TAG} -b pkg-build/amd64/ -o go-bin-rpm-amd64.rpm
+TAG=$1
+if [[ -z ${TAG} ]]; then TAG="0.0.0"; fi
+VERBOSE=* ./go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o go-bin-rpm-386.rpm
+VERBOSE=* ./go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o go-bin-rpm-amd64.rpm
