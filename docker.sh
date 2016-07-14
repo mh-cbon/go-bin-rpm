fe2b96ea3d02003cc2940c2f945f2241e36028c2
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-14 19:13:33 +0200

    travis: update build script

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
-
 dnf install rpm-build -y
 cd /docker
 TAG=$1
+NAME=$2
 if [[ -z ${TAG} ]]; then TAG="0.0.0"; fi
-VERBOSE=* ./go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o go-bin-rpm-386.rpm
-VERBOSE=* ./go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o go-bin-rpm-amd64.rpm
+VERBOSE=* ./go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o ${NAME}-386.rpm
+VERBOSE=* ./go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o ${NAME}-amd64.rpm
