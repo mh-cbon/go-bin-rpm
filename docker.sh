b16295a5748a85befe04322ead7076cbb1d6f716
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-14 13:54:37 +0200

    travis: update

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 
 dnf install rpm-build -y
 cd /docker
+TRAVIS_TAG=0.0.1
 VERBOSE=* ./go-bin-rpm generate -a 386 --version ${TRAVIS_TAG} -b pkg-build/386/ -o go-bin-rpm-386.rpm
 VERBOSE=* ./go-bin-rpm generate -a amd64 --version ${TRAVIS_TAG} -b pkg-build/amd64/ -o go-bin-rpm-amd64.rpm
