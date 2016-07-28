2e4b6b1df14d2d3f2d91463dc50d1bd2d8b42c24
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-28 09:49:14 +0200

    support: add centralized script to generate packages and repository

---
+++ create-pkg.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+# this is an helper
+# to use into your travis file
+# it is limited to amd64/386 arch
+#
+# to use it
+# curl -L https://raw.githubusercontent.com/mh-cbon/go-bin-rpm/master/create-pkg.sh \
+# | GH=mh-cbon/gh-api-cli sh -xe
+
+REPO=`echo ${GH} | cut -d '/' -f 2`
+USER=`echo ${GH} | cut -d '/' -f 1`
+
+
+cat <<EOT > docker.sh
+dnf install rpm-build -y
+
+curl -L https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh \
+| GH=mh-cbon/changelog sh -xe
+
+curl -L https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh \
+| GH=mh-cbon/go-bin-rpm sh -xe
+
+cd /docker
+TAG=$1
+NAME=$2
+if [[ -z ${TAG} ]]; then TAG="0.0.0"; fi
+VERBOSE=* go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o ${NAME}-386.rpm
+VERBOSE=* go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o ${NAME}-amd64.rpm
+EOT
+
+rm -fr pkg-build/*
+docker run -v $PWD:/docker fedora /bin/sh -c "cd /docker && sh ./docker.sh ${TRAVIS_TAG} ${REPO}"
+sudo chown travis:travis ${REPO}-{386,amd64}.rpm
---
+++ setup-repository.sh
@@ -0,0 +1,67 @@
+#!/bin/sh -e
+
+# this is an helper
+# to use into your travis file
+# it is limited to amd64/386 arch
+#
+# to use it
+# curl -L https://raw.githubusercontent.com/mh-cbon/go-bin-rpm/master/setup-deb-repository.sh \
+# | GH=mh-cbon/gh-api-cli EMAIL=mh-cbon@users.noreply.github.com sh -xe
+
+# GH=$1
+# EMAIL=$2
+
+REPO=`echo ${GH} | cut -d '/' -f 2`
+USER=`echo ${GH} | cut -d '/' -f 1`
+
+sudo apt-get install build-essential -y
+
+if type "gh-api-cli" > /dev/null; then
+  echo "gh-api-cli already installed"
+else
+  curl -L https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh | GH=mh-cbon/gh-api-cli sh -xe
+fi
+
+git checkout --track -b origin/gh-pages || echo "branch already exists"
+git config user.name "${USER}"
+git config user.email "${EMAIL}"
+
+rm -fr rpm
+mkdir -p rpm/{i386,x86_64}
+gh-api-cli dl-assets -o ${USER} -r ${REPO} --out rpm/i386/%r-%v_%a.%e -g "*386*rpm"
+gh-api-cli dl-assets -o ${USER} -r ${REPO} --out rpm/x86_64/%r-%v_%a.%e -g "*amd64*rpm"
+
+cat <<EOT > createrepo.sh
+yum install createrepo -y
+cd /docker/rpm/i386
+createrepo .
+cd /docker/rpm/x86_64
+createrepo .
+EOT
+docker run -v $PWD:/docker fedora /bin/sh -c "cd /docker && sh ./createrepo.sh"
+
+
+cat <<EOT > gen-repo-file.sh
+DESC=`rpm -qip *.rpm | grep Summary | cut -d ':' -f2 | cut -d ' ' -f2- | tail -n 1`
+cat <<EOTin > rpm/${REPO}.repo
+[${REPO}]
+name=${DESC}
+baseurl=https://${USER}.github.io/${REPO}/rpm/\$basearch/
+enabled=1
+skip_if_unavailable=1
+gpgcheck=0
+EOTin
+EOT
+docker run -v $PWD:/docker fedora /bin/sh -c "cd /docker && sh ./gen-repo-file.sh"
+
+rm -f gen-repo-file.sh
+rm -f createrepo.sh
+
+
+git add -A
+git commit -m "Created rpm repository"
+
+set +x # disable debug output because that would display the token in clear text..
+echo "git push -u --force --quiet https://GH_TOKEN@github.com/${GH}.git origin gh-pages"
+git push -u --force --quiet "https://${GH_TOKEN}@github.com/${GH}.git" origin gh-pages \
+ 2>&1 | sed -re "s/${GH_TOKEN}/GH_TOKEN/g"
