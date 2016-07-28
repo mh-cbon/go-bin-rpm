142ec3dd0a0b6253e941a14c4a435212f64244c0
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-28 10:00:01 +0200

    support: fix variable excaping in docker script

--- create-pkg.sh
+++ create-pkg.sh
@@ -24,9 +24,9 @@ curl -L https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh \
 cd /docker
 TAG=$1
 NAME=$2
-if [[ -z ${TAG} ]]; then TAG="0.0.0"; fi
-VERBOSE=* go-bin-rpm generate -a 386 --version ${TAG} -b pkg-build/386/ -o ${NAME}-386.rpm
-VERBOSE=* go-bin-rpm generate -a amd64 --version ${TAG} -b pkg-build/amd64/ -o ${NAME}-amd64.rpm
+if [[ -z \${TAG} ]]; then TAG="0.0.0"; fi
+VERBOSE=* go-bin-rpm generate -a 386 --version \${TAG} -b pkg-build/386/ -o \${NAME}-386.rpm
+VERBOSE=* go-bin-rpm generate -a amd64 --version \${TAG} -b pkg-build/amd64/ -o \${NAME}-amd64.rpm
 EOT
 
 rm -fr pkg-build/*
