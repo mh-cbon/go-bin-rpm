3556983fb5879ba87ecc5b6ec3ce027b03adc912
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-20 13:11:50 +0200

    do not build/upload binaries

--- .version.sh
+++ .version.sh
@@ -15,18 +15,10 @@ PREVERSION=
   666 commit -q -m "README: !newversion!" -f README.md
   666 emd gen -in go-bin-rpm-utils/README.e.md -out go-bin-rpm-utils/README.md
   666 commit -q -m "README: !newversion!" -f go-bin-rpm-utils/README.md
-  666 rm-glob -r build/
-  666 rm-glob -r assets/
-  666 build-them-all build main.go -o "build/&os-&arch/&pkg" --ldflags "-X main.VERSION=!newversion!"
 
 POSTVERSION=
   666 git push
   666 git push --tags
-  philea -s -S -p "build/windows*/*" "666 archive create -f -o=assets/%dname.zip -C=build/%dname/ ."
-  philea -s -S -e windows*/** -p "build/*/**" "666 archive create -f -o=assets/%dname.tar.gz -C=build/%dname/ ."
   666 gh-api-cli create-release -n release -o mh-cbon -r go-bin-rpm \
     --ver !newversion! -c "changelog ghrelease --version !newversion!" \
     --draft !isprerelease!
-  666 gh-api-cli upload-release-asset -n release --glob "assets/*" -o mh-cbon -r go-bin-rpm --ver !newversion!
-  666 rm-glob -r build/
-  666 rm-glob -r assets/
