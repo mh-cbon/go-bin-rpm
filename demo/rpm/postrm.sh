b8418b0edc1a4f3be98d7b03224ebb8dd326714b
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-20 16:13:31 +0000

    clean

--- demo/rpm/postrm.sh
+++
@@ -1,4 +0,0 @@
-echo "postrm"
-
-# not sure what this does...
-%systemd_postun_with_restart hello.service
--- demo/rpm/prerm.sh
+++
@@ -1,7 +0,0 @@
-echo "prerm"
-
-# non standard stuff, stop the service asap
-systemctl stop hello.service
-
-# unregister the service
-%systemd_preun hello.service
